43fb051b3501e763033cdf5a0af027a0726ab68c
Igor Bomfim <149107766+igorbweb@users.noreply.github.com>
2026-07-27 13:45:06 -0300

    Update page-contato.php

--- wp-content/themes/nova-bairros/page-contato.php
+++ wp-content/themes/nova-bairros/page-contato.php
@@ -141,7 +141,7 @@ get_header(); ?>
                     0: { slidesPerView: 1.2 },
                     768: { slidesPerView: 2 },
                     1024: { slidesPerView: 4 },
-                    1400: { slidesPerView: 'auto' }
+                    1400: { slidesPerView: 5 }
                 },
                 pagination: {
                     el: '.swiper-pagination',
