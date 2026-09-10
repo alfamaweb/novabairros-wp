e03c81d84d26c292c127f522ecf51ecfe2db6989
Carlos Augusto <carlosaugustosg@gmail.com>
2026-09-10 11:23:33 -0300

    ajuste posição dos elementos diferenciais pt2

--- wp-content/themes/nova-bairros/page-home.php
+++ wp-content/themes/nova-bairros/page-home.php
@@ -569,7 +569,7 @@ get_header(); ?>
         <section class="sobre">
             <div class="container">
                 <div class="flex flex-col md:grid md:grid-cols-12 items-center justify-center content-center gap-x-7 gap-y-8">
-                    <div class="col-span-12 md:col-span-6 wow fadeInLeft">
+                    <div class="col-span-12 md:col-span-12 wow fadeInLeft">
                         <div class="flex flex-col items-start w-fit gap-3">
                             <span>SOBRE NÓS</span>
                             <h2><?= $sobre['titulo']; ?></h2>
