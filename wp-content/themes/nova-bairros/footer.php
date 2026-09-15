dc103519c4c202cd781e2cb82b429a497e8f47bd
Igor Bomfim <149107766+igorbweb@users.noreply.github.com>
2026-09-14 22:47:20 -0300

    Update footer.php

--- wp-content/themes/nova-bairros/footer.php
+++ wp-content/themes/nova-bairros/footer.php
@@ -118,7 +118,7 @@ $desenvolvido_por    = $footer_opts['desenvolvido_por'] ?? [];
         <!-- Barra de copyright -->
         <div class="border-t border-white/20 flex justify-between items-center py-6 wow fadeIn" data-wow-delay="0.2s">
             <div class="flex flex-col md:flex-row text-center text-sm items-center gap-5 justify-between w-full">
-                <p class="!text-white/60">©<?= date('Y'); ?>Nova Bairros - Todos os direitos reservados.</p>
+                <p class="!text-white/60">©<?= date('Y'); ?> Nova Bairros - Todos os direitos reservados.</p>
                 <!-- <div class="hidden md:block w-[1px] h-4 bg-white/20 mx-3"></div> -->
                 <!-- <a class="!text-white/60 font-medium !underline" href="#">Política de Privacidade</a>
                 <div class="hidden md:block w-[1px] h-4 bg-white/20 mx-3"></div> -->
